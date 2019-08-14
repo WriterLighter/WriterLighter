3dad9f9e97ba4b75e585f976f37a3bcd839a4d18
Yugo Yagita <2000ygkn0713@gmail.com>
2019-08-14 23:47:42 +0900

    add editor domain

---
+++ src/states/editor/stores.ts
@@ -0,0 +1,38 @@
+import { convertFromRaw, EditorState } from 'draft-js';
+import { openedNovel } from './../novels/stores';
+import { editorDomain } from './domain';
+import { change, load, open } from './events';
+
+export type Editor =
+  | { id: number }
+  | {
+    id: number;
+    partId: number;
+    editorState: EditorState;
+  };
+
+export const editors = editorDomain
+  .store<Editor[]>([])
+  .on(open, state => [...state, { id: state.length }])
+  .on(load, (state, { editorId, partId }) => {
+    const novel = openedNovel.getState();
+    if (!novel) {
+      throw new Error('novel is not opened.');
+    }
+    if (!novel.parts[partId]) {
+      throw new Error(
+        `partId: ${partId} is not found in novel: ${novel.title}`,
+      );
+    }
+    const editorState = EditorState.createWithContent(
+      convertFromRaw(novel.parts[partId]),
+    );
+    return state.map((editor, i) =>
+      i === editorId ? { ...editor, editorState } : editor,
+    );
+  })
+  .on(change, (state, { editorState, editorId }) =>
+    state.map(editor =>
+      editor.id === editorId ? { ...editor, editorState } : editor,
+    ),
+  );
