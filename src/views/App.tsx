ae808c08d62e72601ee0d0f5e92ee7c784467e55
Yugo Yagita <2000ygkn0713@gmail.com>
2019-08-16 21:28:33 +0900

    I moved it for the time being

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,10 +1,37 @@
-import styled from '@emotion/styled';
+import { Editor, EditorState } from 'draft-js';
+import { useStore } from 'effector-react';
 import * as React from 'react';
+import database from '../database';
+import { change, load, open as openEditor } from '../states/editor/events';
+import { editors as editorsStore } from '../states/editor/stores';
+import { createNovel, open as openNovel } from '../states/novels/events';
 
-const Header = styled('h1')`
-  color: tomato;
-`;
+const App = () => {
+  const editor = useStore(editorsStore)[0];
+  const onChange = React.useCallback(
+    (editorState: EditorState) => change({ editorId: 0, editorState }),
+    [],
+  );
 
-const App = () => <Header>Hello!</Header>;
+  React.useEffect(() => {
+    (async () => {
+      if ((await database.novels.count()) === 0) {
+        await createNovel({
+          title: 'WriterLighterへようこそ！',
+          author: 'WriterLighter',
+        });
+      }
+
+      await openNovel({ id: 1 });
+      openEditor();
+      load({ partId: 0, editorId: 0 });
+    })();
+  },              []);
+
+  if (editor == null || editor.editorState == null) {
+    return <span>loading...</span>;
+  }
+  return <Editor editorState={editor.editorState} onChange={onChange} />;
+};
 
 export default App;
