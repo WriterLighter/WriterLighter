e039ad28b9ab3be77d3eaace9464e71088f0c7d3
ygkn <2000ygkn0713@gmail.com>
2020-12-26 04:44:20 +0900

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jsx-a11y'],
+  extends: [
+    // base
+    'eslint:recommended',
+
+    // React
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+
+    // TypeScript
+    'plugin:@typescript-eslint/recommended',
+
+    // import
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+
+    // Prettier integration
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/destructuring-assignment': 'off',
+
+    // for Next.js
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['href'],
+      },
+    ],
+
+    // for TypeScript
+    'react/prop-types': 'off',
+
+    // restrict order of imports
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'parent', ['sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
