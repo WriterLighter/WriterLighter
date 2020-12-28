4f270c30cc7f80ae6bda28446fac5c08e98630e6
ygkn <2000ygkn0713@gmail.com>
2020-12-28 16:42:17 +0900

    Add Logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,28 @@
+import { Box, Button, ButtonGroup, Flex } from '@chakra-ui/react';
+import Head from 'next/head';
 import { VFC } from 'react';
 
 import { Editor } from '../components/Editor/Editor';
 import { useEditorState } from '../components/Editor/useEditorState';
+import { Logo } from '../components/Logo';
 
 const IndexPage: VFC = () => {
   const editor = useEditorState();
 
   return (
-    <div className="w-screen h-screen flex flex-col">
-      <div className="p-2">
-        <button
-          type="button"
-          onClick={() => editor.onChange(`${editor.value}hoge`)}
-          className="px-2 py-1 bg-blue-500 text-white rounded mr-4"
-        >
-          edit (add `hoge` to tail)
-        </button>
-        <button
-          type="button"
-          onClick={editor.toggleDirection}
-          className="px-2 py-1 bg-blue-500 text-white rounded mr-4"
-        >
-          toggle
-        </button>
-      </div>
-      <div className="flex-grow overflow-scroll">
+    <Flex w="full" h="full" flexDir="column">
+      <Head>
+        <title>WriterLighter</title>
+      </Head>
+      <ButtonGroup p="2">
+        <Button variant="ghost" h="10" p="2">
+          <Logo />
+        </Button>
+      </ButtonGroup>
+      <Box flexGrow={1} overflow="scroll">
         <Editor {...editor} />
-      </div>
-    </div>
+      </Box>
+    </Flex>
   );
 };
 
