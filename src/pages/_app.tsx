421e3ee1ea6da315090add765549d8b1a75057a5
ygkn <2000ygkn0713@gmail.com>
2021-03-02 11:22:37 +0900

    add viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { theme } from '../theme';
 const MyApp: VFC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <Component {...pageProps} />
     </ChakraProvider>
   );
